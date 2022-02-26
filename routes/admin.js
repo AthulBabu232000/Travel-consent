69805f0ff901098968d3e43a3fa66fcff37ca615
webdesign4alll <190048@tkmce.ac.in>
2022-02-26 23:49:29 +0530

    final-commit-this-day

--- routes/admin.js
+++ routes/admin.js
@@ -10,6 +10,33 @@ const consentUsers=[
   "Me"
 ];
 
+const userConsentDetails=[
+  {
+    id:12,
+    name:consentUsers[0],
+    occupation:'government',
+    consent:'I just need to see what actually happen to the world and the real nature of mankind'
+  },
+  {
+    id:13,
+    name:consentUsers[1],
+    occupation:'government',
+    consent:'I just need to see what actually happen to the world and the real nature of mankind'
+  },
+  {
+    id:14,
+    name:consentUsers[2],
+    occupation:'government',
+    consent:'I just need to see what actually happen to the world and the real nature of mankind'
+  },
+  {
+    id:15,
+    name:consentUsers[3],
+    occupation:'government',
+    consent:'I just need to see what actually happen to the world and the real nature of mankind'
+  },
+]
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.render('admin/show-index', {title: 'Express', admin:true, consentUsers:consentUsers});
@@ -18,7 +45,7 @@ router.get('/show-user', function(req, res, next) {
   res.render('admin/show-user', {title: 'Express', admin:true, consentUsers:consentUsers});
 });
 router.get('/user-consent-details', function(req, res, next) {
-  res.render('admin/user-consent-details', {title: 'Express', admin:true, consentUsers:consentUsers});
+  res.render('admin/user-consent-details', {title: 'Express', admin:true, consentUsers:consentUsers,userConsentDetails});
 });
 
 
